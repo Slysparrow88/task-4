9340c1d2d648cf59947446aa840e6005b07eed81
Slysparrow88 <pavlusha198806@gmail.com>
2019-07-31 15:08:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnUp = document.querySelector('#up');
+const btnDown = document.querySelector('#down');
+
+const osnArria = document.querySelector('#osnArria');
+const block = document.querySelector('#block');
+
+var pos = 0;
+
+btnRight.onclick = function goLeft(){
+    var idRight = setTimeout(goLeft, 10);
+    if (pos === 300){
+        clearInterval(idRight);
+    }else {
+        pos++;
+        block.style.left = pos + 'px';
+    }
+    console.log(idRight);
+}
+
+btnLeft.onclick = function goRight(){
+    var idLeft = setTimeout(goRight, 10);
+    if (pos === -300){
+        clearInterval(idLeft);
+    }else {
+        pos--;
+        block.style.left = pos + 'px';
+    }
+    console.log(idLeft);
+}
+
+btnUp.onclick = function goUp(){
+    var idUp = setTimeout(goUp, 10);
+    if (pos === -300){
+        clearInterval(idUp);
+    }else {
+        pos--;
+        block.style.top = pos + 'px';
+    }
+    console.log(idUp);
+}
+
+btnDown.onclick = function goDown(){
+    var idDown = setTimeout(goDown, 10);
+    if (pos === 300){
+        clearInterval(idDown);
+    }else {
+        pos++;
+        block.style.top = pos + 'px';
+    }
+    console.log(idDown);
+}

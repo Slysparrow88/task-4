8e94bafd24405e6c6ce9a094d7961a7e304c7f66
Slysparrow88 <pavlusha198806@gmail.com>
2019-08-02 12:14:58 +0300

    'second'

--- main.js
+++ main.js
@@ -5,49 +5,56 @@ const btnDown = document.querySelector('#down');
 
 const osnArria = document.querySelector('#osnArria');
 const block = document.querySelector('#block');
+const reset = document.querySelector('#reset');
 
 var pos = 0;
 
-btnRight.onclick = function goLeft(){
+btnRight.onclick = function goLeft() {
     var idRight = setTimeout(goLeft, 10);
-    if (pos === 300){
+    if (pos === 300) {
         clearInterval(idRight);
-    }else {
+    } else {
         pos++;
         block.style.left = pos + 'px';
     }
     console.log(idRight);
 }
 
-btnLeft.onclick = function goRight(){
+btnLeft.onclick = function goRight() {
     var idLeft = setTimeout(goRight, 10);
-    if (pos === -300){
+    if (pos === -300) {
         clearInterval(idLeft);
-    }else {
+    } else {
         pos--;
         block.style.left = pos + 'px';
     }
     console.log(idLeft);
 }
 
-btnUp.onclick = function goUp(){
+btnUp.onclick = function goUp() {
     var idUp = setTimeout(goUp, 10);
-    if (pos === -300){
+    if (pos === -300) {
         clearInterval(idUp);
-    }else {
+    } else {
         pos--;
         block.style.top = pos + 'px';
     }
     console.log(idUp);
 }
 
-btnDown.onclick = function goDown(){
+btnDown.onclick = function goDown() {
     var idDown = setTimeout(goDown, 10);
-    if (pos === 300){
+    if (pos === 300) {
         clearInterval(idDown);
-    }else {
+    } else {
         pos++;
         block.style.top = pos + 'px';
     }
     console.log(idDown);
+}
+
+reset.onclick = function() {
+    block.style = 0;
+    pos = 0; 
+    console.log(pos)
 }
